dcc54c20acc067e09740963649f2285671e5d4c3
RichardObreme <obremerichard@gmail.com>
2025-04-14 15:19:50 +0200

    navbar component

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+/* type SignInPageProps = {
+  
+}; */
+
+export default function SignInPage(/* {  }: SignInPageProps */) {
+  return <div>SignInPage</div>;
+}
---
+++ src/app/(consumer)/layout.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function ConsumerLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
+function Navbar() {
+  return (
+    <header className="flex h-12 shadow bg-background z-10">
+      <nav className="flex gap-4 container">
+        <Link
+          className="mr-auto text-lg hover:underline px-2 flex items-center"
+          href={"/"}
+        >
+          Web Dev Simplified
+        </Link>
+        <>
+          {/* //TODO: 
+            //! ONLY For logged user 
+        */}
+          <Link
+            className="hover:bg-accent/10 flex items-center px-2"
+            href="/admin"
+          >
+            Admin
+          </Link>
+          <Link
+            className="hover:bg-accent/10 flex items-center px-2"
+            href="/courses"
+          >
+            My Courses
+          </Link>
+          <Link
+            className="hover:bg-accent/10 flex items-center px-2"
+            href="/purchases"
+          >
+            Purchase History
+          </Link>
+          <div className="size-8 self-center">
+            <button>Sign-in</button>
+          </div>
+          <div className="size-8 self-center">
+            <button>Sign-out</button>
+          </div>
+        </>
+      </nav>
+    </header>
+  );
+}
