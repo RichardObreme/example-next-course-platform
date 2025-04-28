ccc58535ab3fc2b934e9673a7d65ace2d0357a5b
RichardObreme <obremerichard@gmail.com>
2025-04-28 16:53:45 +0200

    getCurrentUser (role)

--- src/features/users/db/users.ts
+++ src/features/users/db/users.ts
@@ -1,3 +1,18 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export async function getCurrentUser() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) return { role: undefined };
+
+  return session.user;
+}
+
 /*************************
  *    Code for Clerk     *
  *************************/
