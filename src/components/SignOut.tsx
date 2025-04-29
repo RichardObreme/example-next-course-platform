dc7f3243cc338d4dfa62119f9897b0d19a8b5cc1
RichardObreme <obremerichard@gmail.com>
2025-04-29 06:49:06 +0200

    fix navbar logout

---
+++ src/components/SignOut.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Button } from "./ui/button";
+
+export default function SignOut() {
+  return <Button onClick={() => authClient.signOut()}>Sign-Out</Button>;
+}
